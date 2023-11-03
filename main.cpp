281cdcccfca2eebacbe5c8fc9b6c2c01741c603c
Hossam Elgendy <59685428+hossamelgendy1@users.noreply.github.com>
2023-11-03 14:57:34 +0200

    Created main.cpp and added initial implementation

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int getSum(const vector<int>& vec){
+    int sum = 0;
+    for(int i : vec) 
+        sum += i;
+    return sum;
+}
+
+int main(){
+    vector<int> v(10);
+    for (int i = 0; i < 10; i++)
+        v[i] = i+1;
+
+    cout << getSum(v) << endl;
+    
+    return 0;
+}
