ccaaac9d91c78946c7ff10bf191867265994dcff
Hossam Elgendy <59685428+hossamelgendy1@users.noreply.github.com>
2023-11-03 15:38:43 +0200

    implemented getAverage() function

--- main.cpp
+++ main.cpp
@@ -9,12 +9,17 @@ int getSum(const vector<int>& vec){
     return sum;
 }
 
+double getAverage(const vector<int>& vec){
+    return 1.0 * getSum(vec) / vec.size();
+}
+
 int main(){
     vector<int> v(10);
     for (int i = 0; i < 10; i++)
         v[i] = i+1;
 
     cout << "vector sum = " << getSum(v) << endl;
-    
+    cout << "vector average = " << getAverage(v) << endl;
+
     return 0;
 }
